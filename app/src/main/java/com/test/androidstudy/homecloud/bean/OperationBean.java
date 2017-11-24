fe60765757df188bc61af9562a93ff81460c2256
ljh <578525306@qq.com>
2017-11-24 13:56:43 +0800

    新增了极光推送实现，可与蓝牙设备交互

---
+++ app/src/main/java/com/test/androidstudy/homecloud/bean/OperationBean.java
@@ -0,0 +1,26 @@
+package com.test.androidstudy.homecloud.bean;
+
+/**
+ * Created by ljh on 2017/11/24.
+ */
+
+public class OperationBean {
+    private Integer id;
+    private Integer operCode;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getOperCode() {
+        return operCode;
+    }
+
+    public void setOperCode(Integer operCode) {
+        this.operCode = operCode;
+    }
+}
